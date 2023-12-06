7fef4df90d5ee143af483291d869fa828d0f3cd4
Christian <Christian.Spelt@student.rocva.nl>
2023-12-07 00:00:55 +0100

    pop up

--- zweetz/index.php
+++ zweetz/index.php
@@ -6,44 +6,68 @@
   <link rel="stylesheet" href="style.css">
   <title>zweetz</title>
 </head>
-<script src="script.js"></script>
+
+
 <body>
-    <header class="nav-balk">
-        <div>a</div>
-        <div class="nav-balk">
+    <header class="nav">
+        <div class="nav-balk">  
+            <div>a</div>
             <div class="nav-items">
                 <a href="#">Home</a>
                 <a href="#">Producten</a>
                 <a href="#">about</a>
+
             </div>
             <div class="winkelkarretje">
-                <img id="karretje" src="img/winkelkartje.png">
+                <img id="karretje" onclick="winkelwagens()" src="img/winkelkartje.png">
             </div>
+
         </div>
+        <div id="winkelwagen">
+                <h2>Winkelwagen</h2>
+                <ul id="winkelwagen-lijst">
+                <!-- Winkelwagenitems worden hier weergegeven -->
+                </ul>
+                <p>Totaal: <span id="totaal-bedrag">0.00</span> </p>
+                <button href="#">Afrekenen</button>
+                <script src="script.js"></script>
+            </div>
     </header>
 
-    <section id="producten">
+    <div id="producten">
         <!-- Producten worden hier weergegeven -->
         <div class="product">
             <h3>Product 1</h3>
-            <p>Prijs: $10.00</p>
+            <p>Prijs: €10.00</p>
             <button onclick="addToCart('Product 1', 10.00)">Voeg toe aan winkelwagen</button>
         </div>
         <div class="product">
             <h3>Product 2</h3>
-            <p>Prijs: $15.00</p>
+            <p>Prijs: €15.00</p>
             <button onclick="addToCart('Product 2', 15.00)">Voeg toe aan winkelwagen</button>
         </div>
-        <!-- Voeg meer producten toe zoals hierboven -->
-    </section>
+        <div class="product">
+            <h3>Product 3</h3>
+            <p>Prijs: €20.00</p>
+            <button onclick="addToCart('Product 3', 20.00)">Voeg toe aan winkelwagen</button>
+        </div>
+        <div class="product">
+            <h3>Product 4</h3>
+            <p>Prijs: €25.00</p>
+            <button onclick="addToCart('Product 4', 25.00)">Voeg toe aan winkelwagen</button>
+        </div>
+        <div class="product">
+            <h3>Product 5</h3>
+            <p>Prijs: €30.00</p>
+            <button onclick="addToCart('Product 5', 30.00)">Voeg toe aan winkelwagen</button>
+        </div>
+        <div class="product">
+            <h3>Product 6</h3>
+            <p>Prijs: €35.00</p>
+            <button onclick="addToCart('Product 6', 35.00)">Voeg toe aan winkelwagen</button>
+        </div>
+    </div>
 
-    <section id="winkelwagen">
-        <h2>Winkelwagen</h2>
-        <ul id="winkelwagen-lijst">
-            <!-- Winkelwagenitems worden hier weergegeven -->
-        </ul>
-        <p>Totaal: <span id="totaal-bedrag">0.00</span> EUR</p>
-        <button href="#">Afrekenen</button>
-    </section>
+
 </body>
 </html>
--- zweetz/script.js
+++ zweetz/script.js
@@ -1,34 +1,45 @@
-var winkelwagen = [];
+   // Winkelwagenobject om de geselecteerde producten op te slaan
+   var winkelwagen = [];
 
-function addToCart(productNaam, productPrijs) {
-    // Voeg het product toe aan de winkelwagen
-    winkelwagen.push({
-        naam: productNaam,
-        prijs: productPrijs
-    });
+   function addToCart(productNaam, productPrijs) {
+       // Voeg het product toe aan de winkelwagen
+       winkelwagen.push({
+           naam: productNaam,
+           prijs: productPrijs
+       });
 
-    // Werk de winkelwagenweergave bij
-    updateCartView();
-}
+       // Werk de winkelwagenweergave bij
+       updateCartView();
+   }
 
-function updateCartView() {
-    var winkelwagenLijst = document.getElementById('winkelwagen-lijst');
-    var totaalBedragElement = document.getElementById('totaal-bedrag');
-    
-    // Wis de huidige weergave
-    winkelwagenLijst.innerHTML = '';
 
-    // Vul de weergave opnieuw in met de bijgewerkte gegevens
-    var totaalBedrag = 0;
-    winkelwagen.forEach(function(item) {
-        var listItem = document.createElement('li');
-        listItem.textContent = item.naam + ' - $' + item.prijs.toFixed(2);
-        winkelwagenLijst.appendChild(listItem);
+   function updateCartView() {
+       var winkelwagenLijst = document.getElementById('winkelwagen-lijst');
+       var totaalBedragElement = document.getElementById('totaal-bedrag');
+       
+       // Wis de huidige weergave
+       winkelwagenLijst.innerHTML = '';
 
-        totaalBedrag += item.prijs;
-    });
+       // Vul de weergave opnieuw in met de bijgewerkte gegevens
+       var totaalBedrag = 0;
+       winkelwagen.forEach(function(item) {
+           var listItem = document.createElement('li');
+           listItem.textContent = item.naam + ' - $' + item.prijs.toFixed(2);
+           winkelwagenLijst.appendChild(listItem);
 
-    // Werk het totaalbedrag bij
-    totaalBedragElement.textContent = totaalBedrag.toFixed(2) + ' EUR';
-}
+           totaalBedrag += item.prijs;
+       });
+
+       // Werk het totaalbedrag bij
+       totaalBedragElement.textContent = totaalBedrag.toFixed(2) + ' EUR';
+   }
+
+   function checkout() {
+       // Plaats hier de code voor het afrekenen (bijvoorbeeld doorgeven aan een backend, verwerken van betalingen, etc.)
+       alert('Bedankt voor uw aankoop!');
+   }
+
+   function winkelwagens(){
+        document.getElementById('winkelwagen').style.display = 'flex'
+   }
 
