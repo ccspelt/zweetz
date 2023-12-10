67fb6143965d08e89c67fbe8d387f000db36bd50
Christian <Christian.Spelt@student.rocva.nl>
2023-12-10 16:57:07 +0100

    producten toegevoegd

--- zweetz/index.php
+++ zweetz/index.php
@@ -52,13 +52,13 @@
                 <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
                 <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
             </div>
-    </div>
 
                 <p>Prijs: €10.00</p>
                 <button onclick="addToCart('Product 1', 10.00)">Voeg toe aan winkelwagen</button>
                 <button class="Btn" onclick="prevSlide(this)">❮</button>
                 <button class="Btn" onclick="nextSlide(this)">❯</button>
             </div>
+            
             <div class="product">
         <h3>Product 2</h3>      
             <div class="slider-container">
@@ -67,17 +67,105 @@
                 <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
                 <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
             </div>
-    </div>
 
                 <p>Prijs: €15.00</p>
                 <button onclick="addToCart('Product 2', 15.00)">Voeg toe aan winkelwagen</button>
                 <button class="Btn" onclick="prevSlide(this)">❮</button>
                 <button class="Btn" onclick="nextSlide(this)">❯</button>
+            </div>    
+            
+            <div class="product">
+        <h3>Product 3</h3>      
+            <div class="slider-container">
+                <div class="slider">
+                <div class="slide"><img class="img_pro" src="img/ik.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
+            </div>
+
+                <p>Prijs: €25.00</p>
+                <button onclick="addToCart('Product 3', 25.00)">Voeg toe aan winkelwagen</button>
+                <button class="Btn" onclick="prevSlide(this)">❮</button>
+                <button class="Btn" onclick="nextSlide(this)">❯</button>
+            </div>        
+            
+            <div class="product">
+        <h3>Product 4</h3>      
+            <div class="slider-container">
+                <div class="slider">
+                <div class="slide"><img class="img_pro" src="img/ik.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
+            </div>
+
+                <p>Prijs: €30.00</p>
+                <button onclick="addToCart('Product 4', 30.00)">Voeg toe aan winkelwagen</button>
+                <button class="Btn" onclick="prevSlide(this)">❮</button>
+                <button class="Btn" onclick="nextSlide(this)">❯</button>
             </div>
             
+            <div class="product">
+        <h3>Product 5</h3>      
+            <div class="slider-container">
+                <div class="slider">
+                <div class="slide"><img class="img_pro" src="img/ik.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
+            </div>
+
+                <p>Prijs: €50.00</p>
+                <button onclick="addToCart('Product 5', 50.00)">Voeg toe aan winkelwagen</button>
+                <button class="Btn" onclick="prevSlide(this)">❮</button>
+                <button class="Btn" onclick="nextSlide(this)">❯</button>
+            </div>                    
+
+            <div class="product">
+        <h3>Product 6</h3>      
+            <div class="slider-container">
+                <div class="slider">
+                <div class="slide"><img class="img_pro" src="img/ik.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
+            </div>
+
+                <p>Prijs: €27.50</p>
+                <button onclick="addToCart('Product 6', 27.50)">Voeg toe aan winkelwagen</button>
+                <button class="Btn" onclick="prevSlide(this)">❮</button>
+                <button class="Btn" onclick="nextSlide(this)">❯</button>
+            </div>
             
+            <div class="product">
+        <h3>Product 7</h3>      
+            <div class="slider-container">
+                <div class="slider">
+                <div class="slide"><img class="img_pro" src="img/ik.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
+            </div>
+
+                <p>Prijs: €99.99</p>
+                <button onclick="addToCart('Product 7', 99.99)">Voeg toe aan winkelwagen</button>
+                <button class="Btn" onclick="prevSlide(this)">❮</button>
+                <button class="Btn" onclick="nextSlide(this)">❯</button>
+            </div> 
+            
+            <div class="product">
+        <h3>Product 8</h3>      
+            <div class="slider-container">
+                <div class="slider">
+                <div class="slide"><img class="img_pro" src="img/ik.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/koe.jpg" ></div>
+                <div class="slide"><img class="img_pro" src="img/Naamloos.jpg"></div>
+            </div>
+
+                <p>Prijs: €5.00</p>
+                <button onclick="addToCart('Product 8', 5.00)">Voeg toe aan winkelwagen</button>
+                <button class="Btn" onclick="prevSlide(this)">❮</button>
+                <button class="Btn" onclick="nextSlide(this)">❯</button>
+            </div>            
         </div>
-    </div>
+
+        
 
 
 </body>
